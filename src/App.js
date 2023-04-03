4a69c3c19cc0f47e523fed1273121004caab8ebb
LiamHepworth <Liammoses1@gmail.com>
2023-04-03 21:30:09 +0100

    Props/data setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
 import './App.css';
 import Card from './components/Card'
+import image from "./images/image-equilibrium.jpg"
+import imageOne from "./images/image-puffer.webp"
+import imageTwo from "./images/image-cubes.webp"
+import imageThree from "./images/image-goop.webp"
+import authorImage from "./images/image-avatar.png"
+
+const data = [
+  {
+    cardImage: image, 
+    cardName: "Equilibrium #32", 
+    cardDesc: "Our Equilibrium collection promotes balance and calm.", 
+    cardPrice: "0.041ETH",
+    cardTimeRemaining: "3 days left",
+    cardAuthorImage: authorImage,
+    cardAuthorName: "Jules Wyvern",
+    id: 1,
+  },
+  {
+    cardImage: imageOne, 
+    cardName: "Unicode #1", 
+    cardDesc: "A collection of 3D glyphs, with a contemporary twist.", 
+    cardPrice: "0.120ETH",
+    cardTimeRemaining: "1 week left",
+    cardAuthorImage: authorImage,
+    cardAuthorName: "Jules Wyvern",
+    id: 2,
+  },
+  {
+    cardImage: imageTwo, 
+    cardName: "Rotations #162", 
+    cardDesc: "Exploring particulate new and unique representations of 3D worldspaces.", 
+    cardPrice: "0.455ETH",
+    cardTimeRemaining: "42 hours left",
+    cardAuthorImage: authorImage,
+    cardAuthorName: "Jules Wyvern",
+    id: 3,
+  },
+  {
+    cardImage: imageThree, 
+    cardName: "Flow #83", 
+    cardDesc: "Otherworldly materials collide in this reinterpretation of urban landscapes", 
+    cardPrice: "0.008ETH",
+    cardTimeRemaining: "6 hours left",
+    cardAuthorImage: authorImage,
+    cardAuthorName: "Jules Wyvern",
+    id: 4,
+  },
+
+]
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card data={data} title="hello"/>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,41 @@
 import eth from '../images/icon-ethereum.svg'
 import timer from '../images/icon-clock.svg'
 
-const Card = () => {
+const Card = ({data}) => {
+
+    console.log(data)
+
     return ( 
-        <div className="card">
-            <div className="container">
-                <img src={require('../images/image-equilibrium.jpg')} alt="artwork" className='card-cover-image'/>
-                <h1 className="card-header">Equilibrium #3429</h1>
-                <span className="card-description">Our Equilibrium collection promotes balance and calm.</span>
 
-                <div className="info">
-                    <div className="pricing">
-                        <img src={eth} alt="eth" className="eth-icon" />
-                        <span className="price">0.041 ETH</span>
+        data.map(data => (
+            <div className="card" key={data.id}>
+                <div className="container">
+                    <img src={data.cardImage} alt="artwork" className='card-cover-image'/>
+                    <h1 className="card-header">{data.cardName}</h1>
+                    <span className="card-description">{data.cardDesc}</span>
+
+                    <div className="info">
+                        <div className="pricing">
+                            <img src={eth} alt="eth" className="eth-icon" />
+                            <span className="price">{data.cardPrice}</span>
+                        </div>
+
+                        <div className="timing">
+                            <img src={timer} alt="timer" className="eth-icon" />
+                            <span className="time">{data.cardTimeRemaining}</span>
+                        </div>
                     </div>
-
-                    <div className="timing">
-                        <img src={timer} alt="timer" className="eth-icon" />
-                        <span className="time">3 days left</span>
+                    
+                    <div className="authors">
+                        <img src={data.cardAuthorImage} alt="" className="author-icon" />
+                        <span className="author-text">Creation of <b>{data.cardAuthorName}</b></span>
                     </div>
                 </div>
-                
-                <div className="authors">
-                    <img src={require('../images/image-avatar.png')} alt="" className="author-icon" />
-                    <span className="author-text">Creation of <b>Jules Wyvern</b></span>
-                </div>
-            </div>
-        </div> 
+            </div> 
+        ))
     );
 }
  
 export default Card;
+
+// cardImage, cardName, cardDesc, cardPrice, cardTimeRemaining, cardAuthorImage, cardAuthorName
